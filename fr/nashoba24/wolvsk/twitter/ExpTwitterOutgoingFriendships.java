10fbc9228b5d6943880cfd5e670f4f173e94528a
Nashoba24 <nicolas.v1999@hotmail.fr>
2016-10-01 12:02:28 +0200

    Add More Twitters functions

---
+++ fr/nashoba24/wolvsk/twitter/ExpTwitterOutgoingFriendships.java
@@ -0,0 +1,49 @@
+package fr.nashoba24.wolvsk.twitter;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.event.Event;
+
+import twitter4j.TwitterException;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+
+public class ExpTwitterOutgoingFriendships extends SimpleExpression<Long>{
+	
+	@Override
+	public boolean isSingle() {
+		return false;
+	}
+	
+	@Override
+	public Class<? extends Long> getReturnType() {
+		return Long.class;
+	}
+	
+	@Override
+	public boolean init(Expression<?>[] expr, int matchedPattern, Kleenean paramKleenean, ParseResult paramParseResult) {
+		return true;
+	}
+	
+	@Override
+	public String toString(@Nullable Event e, boolean paramBoolean) {
+		return "outgoing friendships";
+	}
+	
+	@Override
+	@Nullable
+	protected Long[] get(Event e) {
+		if(WolvSKTwitter.tf==null) { return null; }
+		try {
+			return ArrayUtils.toObject(WolvSKTwitter.tf.getInstance().getOutgoingFriendships(-1).getIDs());
+		} catch (TwitterException e1) {
+			e1.printStackTrace();
+			System.out.println("Failed to get outgoing friendships: " + e1.getMessage());
+			return null;
+		}
+	}
+}
+
--- fr/nashoba24/wolvsk/twitter/ExprTwitterGetDirectMessages.java
+++ fr/nashoba24/wolvsk/twitter/ExprTwitterGetDirectMessages.java
@@ -1,18 +1,21 @@
 package fr.nashoba24.wolvsk.twitter;
 
+import java.util.ArrayList;
+
 import javax.annotation.Nullable;
 
 import org.bukkit.event.Event;
 
 import twitter4j.DirectMessage;
 import twitter4j.Paging;
+import twitter4j.ResponseList;
 import twitter4j.TwitterException;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 
-public class ExprTwitterGetDirectMessages extends SimpleExpression<DirectMessage>{
+public class ExprTwitterGetDirectMessages extends SimpleExpression<String>{
 	
 	private Expression<Integer> page;
 	private boolean paging = false;
@@ -23,8 +26,8 @@ public class ExprTwitterGetDirectMessages extends SimpleExpression<DirectMessage
 	}
 	
 	@Override
-	public Class<? extends DirectMessage> getReturnType() {
-		return DirectMessage.class;
+	public Class<? extends String> getReturnType() {
+		return String.class;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -44,14 +47,28 @@ public class ExprTwitterGetDirectMessages extends SimpleExpression<DirectMessage
 	
 	@Override
 	@Nullable
-	protected DirectMessage[] get(Event e) {
+	protected String[] get(Event e) {
 		if(WolvSKTwitter.tf==null) { return null; }
 		try {
 			if(paging) {
-				return (DirectMessage[]) WolvSKTwitter.tf.getInstance().getDirectMessages(new Paging(page.getSingle(e))).toArray();
+				ResponseList<DirectMessage> list = WolvSKTwitter.tf.getInstance().getDirectMessages(new Paging(page.getSingle(e)));
+				ArrayList<String> msg = new ArrayList<String>();
+				for(DirectMessage dm : list) {
+					msg.add(dm.getSenderScreenName() + "-" + dm.getText());
+				}
+				String[] l = new String[msg.size()];
+				l = msg.toArray(l);
+				return l;
 			}
 			else {
-				return (DirectMessage[]) WolvSKTwitter.tf.getInstance().getDirectMessages().toArray();
+				ResponseList<DirectMessage> list = WolvSKTwitter.tf.getInstance().getDirectMessages();
+				ArrayList<String> msg = new ArrayList<String>();
+				for(DirectMessage dm : list) {
+					msg.add("@" + dm.getSenderScreenName() + " - " + dm.getText());
+				}
+				String[] l = new String[msg.size()];
+				l = msg.toArray(l);
+				return l;
 			}
 		} catch (TwitterException e1) {
 			e1.printStackTrace();
