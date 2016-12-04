651eb78c68cf9d7cfd07d8836d0407d54b08ff58
Nashoba24 <nicolas.v1999@hotmail.fr>
2016-12-04 19:31:00 +0100

    Add maths

---
+++ fr/nashoba24/wolvsk/maths/ExprMathsAbsoluteValue.java
@@ -0,0 +1,44 @@
+package fr.nashoba24.wolvsk.maths;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.event.Event;
+
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+
+public class ExprMathsAbsoluteValue extends SimpleExpression<Double> {
+	
+	private Expression<Number> nb;
+	
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+	
+	@Override
+	public Class<? extends Double> getReturnType() {
+		return Double.class;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] expr, int matchedPattern, Kleenean paramKleenean, ParseResult paramParseResult) {
+		nb = (Expression<Number>) expr[0];
+		return true;
+	}
+	
+	@Override
+	public String toString(@Nullable Event e, boolean paramBoolean) {
+		return "absolute value";
+	}
+	
+	@Override
+	@Nullable
+	protected Double[] get(Event e) {
+        return new Double[]{ Math.abs(nb.getSingle(e).doubleValue()) };
+	}
+}
+
