8c2df712577c1f528f2c0a9900211ef880c9cc18
Nashoba24 <nicolas.v1999@hotmail.fr>
2017-01-21 15:51:53 +0100

    Fix calculate level

--- fr/nashoba24/wolvsk/askyblock/EffASkyBlockCalculateLevel.java
+++ fr/nashoba24/wolvsk/askyblock/EffASkyBlockCalculateLevel.java
@@ -2,10 +2,14 @@ package fr.nashoba24.wolvsk.askyblock;
 
 import javax.annotation.Nullable;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-
-import com.wasteofplastic.askyblock.ASkyBlockAPI;
+import com.wasteofplastic.askyblock.ASkyBlock;
+import com.wasteofplastic.askyblock.LevelCalc;
+import com.wasteofplastic.askyblock.LevelCalcByChunk;
+import com.wasteofplastic.askyblock.Settings;
 
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
@@ -30,6 +34,17 @@ public class EffASkyBlockCalculateLevel extends Effect {
 	
 	@Override
 	protected void execute(Event e) {
-		ASkyBlockAPI.getInstance().calculateIslandLevel(player.getSingle(e).getUniqueId());
+		//ASkyBlockAPI.getInstance().calculateIslandLevel(player.getSingle(e).getUniqueId());
+        if (Settings.fastLevelCalc) {
+            new LevelCalcByChunk(ASkyBlock.getPlugin(), player.getSingle(e).getUniqueId(), Bukkit.getServer().getConsoleSender(), false);
+        } else {
+            if (ASkyBlock.getPlugin().isCalculatingLevel()) {
+            	Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + ASkyBlock.getPlugin().myLocale().islanderrorLevelNotReady);
+                return;
+            }
+            ASkyBlock.getPlugin().setCalculatingLevel(true);
+            LevelCalc levelCalc = new LevelCalc(ASkyBlock.getPlugin(), player.getSingle(e).getUniqueId(), Bukkit.getServer().getConsoleSender(), false);
+            levelCalc.runTaskTimer(ASkyBlock.getPlugin(), 0L, 5L);
+        }
 	}
 }
