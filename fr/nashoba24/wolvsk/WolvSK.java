fef1ee6db3731fe016a3b4a5a86a032c256431a2
Nashoba24 <nicolas.v1999@hotmail.fr>
2017-01-29 15:12:19 +0100

    Add files via upload

--- fr/nashoba24/wolvsk/WolvSK.java
+++ fr/nashoba24/wolvsk/WolvSK.java
@@ -7,9 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.theholywaffle.teamspeak3.TS3Api;
-import com.github.theholywaffle.teamspeak3.TS3Query;
-
 import ch.njol.skript.Skript;
 import fr.nashoba24.wolvsk.askyblock.WolvSKASkyBlock;
 import fr.nashoba24.wolvsk.essentials.WolvSKEssentials;
@@ -21,15 +18,11 @@ import fr.nashoba24.wolvsk.playerpoints.WolvSKPlayerPoints;
 import fr.nashoba24.wolvsk.pvparena.WolvSKPvpArena;
 import fr.nashoba24.wolvsk.serverquery.WolvSKPing;
 import fr.nashoba24.wolvsk.supertrails.WolvSKSuperTrails;
-import fr.nashoba24.wolvsk.teamspeak.WolvSKTS3;
-import fr.nashoba24.wolvsk.twitter.WolvSKTwitter;
 import fr.nashoba24.wolvsk.wolvmc.WolvSKWolvMC;
 
 public class WolvSK extends JavaPlugin implements Listener {
 	
 	private static WolvSK instance;
-	public static TS3Query ts3query;
-	public static TS3Api ts3api;
 	public static HashMap<String, Long> cooldowns = new HashMap<String, Long>();
 	  
 	  @Override
@@ -58,10 +51,8 @@ public class WolvSK extends JavaPlugin implements Listener {
 		   WolvSKPvpArena.registerAll();
 		   WolvSKPing.registerAll();
 		   WolvSKSuperTrails.registerAll();
-		   WolvSKTS3.registerAll();
 		   WolvSKWolvMC.registerAll();
 		   WolvSKPlayerPoints.registerAll();
-		   WolvSKTwitter.registerAll();
 		   WolvSKMaths.registerAll();
 		   Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', "&aWolvSK Enabled!"));
 	  }
